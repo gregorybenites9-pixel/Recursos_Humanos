4eb8205c08acba57cf32db53131576d49770b908
07CHMK1PA111A02 <07CHMK1PA111A02@07CHMK1PA111A02>
2026-05-05 08:53:49 -0500

    LOGIN - DISEÑO COMPLETO - 5 de mayo - 8:53 am

--- Menu_General1/LOGIN.Designer.cs
+++ Menu_General1/LOGIN.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LOGIN));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.txtuser = new System.Windows.Forms.TextBox();
             this.txtpass = new System.Windows.Forms.TextBox();
@@ -39,7 +40,6 @@
             this.linkpass = new System.Windows.Forms.LinkLabel();
             this.btnminimizar = new FontAwesome.Sharp.IconButton();
             this.btncerrar = new FontAwesome.Sharp.IconButton();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -53,10 +53,23 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(250, 330);
             this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(226, 108);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Enabled = false;
             this.panel2.Location = new System.Drawing.Point(318, 92);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(408, 2);
@@ -71,8 +84,10 @@
             this.txtuser.Location = new System.Drawing.Point(318, 66);
             this.txtuser.Name = "txtuser";
             this.txtuser.Size = new System.Drawing.Size(408, 20);
-            this.txtuser.TabIndex = 2;
+            this.txtuser.TabIndex = 1;
             this.txtuser.Text = "USUARIO";
+            this.txtuser.Enter += new System.EventHandler(this.txtuser_Enter);
+            this.txtuser.Leave += new System.EventHandler(this.txtuser_Leave);
             // 
             // txtpass
             // 
@@ -83,12 +98,15 @@
             this.txtpass.Location = new System.Drawing.Point(318, 140);
             this.txtpass.Name = "txtpass";
             this.txtpass.Size = new System.Drawing.Size(408, 20);
-            this.txtpass.TabIndex = 3;
+            this.txtpass.TabIndex = 2;
             this.txtpass.Text = "CONTRASEÑA";
+            this.txtpass.Enter += new System.EventHandler(this.txtpass_Enter);
+            this.txtpass.Leave += new System.EventHandler(this.txtpass_Leave);
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.DimGray;
+            this.panel3.Enabled = false;
             this.panel3.Location = new System.Drawing.Point(318, 163);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(408, 2);
@@ -117,7 +135,7 @@
             this.btnlogin.Location = new System.Drawing.Point(318, 225);
             this.btnlogin.Name = "btnlogin";
             this.btnlogin.Size = new System.Drawing.Size(408, 40);
-            this.btnlogin.TabIndex = 5;
+            this.btnlogin.TabIndex = 3;
             this.btnlogin.Text = "ACCEDER";
             this.btnlogin.UseVisualStyleBackColor = false;
             // 
@@ -130,7 +148,7 @@
             this.linkpass.Location = new System.Drawing.Point(434, 277);
             this.linkpass.Name = "linkpass";
             this.linkpass.Size = new System.Drawing.Size(182, 17);
-            this.linkpass.TabIndex = 6;
+            this.linkpass.TabIndex = 0;
             this.linkpass.TabStop = true;
             this.linkpass.Text = "¿Ha olvidado contraseña?";
             // 
@@ -149,6 +167,7 @@
             this.btnminimizar.Size = new System.Drawing.Size(15, 15);
             this.btnminimizar.TabIndex = 8;
             this.btnminimizar.UseVisualStyleBackColor = false;
+            this.btnminimizar.Click += new System.EventHandler(this.btnminimizar_Click);
             // 
             // btncerrar
             // 
@@ -165,17 +184,7 @@
             this.btncerrar.Size = new System.Drawing.Size(15, 15);
             this.btncerrar.TabIndex = 7;
             this.btncerrar.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(12, 108);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(226, 108);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
             // 
             // LOGIN
             // 
@@ -198,6 +207,7 @@
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LOGIN";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LOGIN_MouseDown);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
